a673eb939b0c6554e40e8cc16b7de1ffc79f40d4
Kanhiya Hurkat <hkanhiya123@gmail.com>
2022-02-08 20:23:20 +0530

    --> CORS policy error issue resolved

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from .database import engine, Base
 from .routers import users, posts, oauth, votes
+from fastapi.middleware.cors import CORSMiddleware
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(users.router, prefix="/user")
