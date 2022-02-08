224f29ec74a136df87173015cbb1578073f2fe09
Kanhiya Hurkat <hkanhiya123@gmail.com>
2022-02-08 22:48:59 +0530

    --> Type set to any for base response schema

--- app/schema/base_schema.py
+++ app/schema/base_schema.py
@@ -11,7 +11,7 @@ class BaseSchema(BaseModel):
         orm_mode = True
 
 class ResponseBaseSchema(BaseSchema, Generic[T]):
-    data: Optional[List[T] | T | None]
+    data: Any
 
 class ErrorBaseSchema(BaseSchema, Generic[T]):
-    error: Optional[object | T | None]
+    error: Any
